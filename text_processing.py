e6528af4812602f2902ba6a834f10fa6cbf06618
LiJA <ljaupup@163.com>
2025-01-05 20:34:27 +0800

    理学院、管理学院、公管学院待处理

--- text_processing.py
+++ text_processing.py
@@ -1,11 +1,17 @@
 import re
 
-conflict_college = ['公共管理', '管理', '理学院']
+conflict_college = ['理学院', '管理', '公共管理']
 
 college_list = ['土木', '环境', '建科', '材料',
-                '管理', '机电', '冶金', '信控',
-                '艺术', '理学', '文学', '资源',
-                '公管', '化工', '体育', '安德', '城市']
+                # '管理',
+                '机电', '冶金', '信控',
+                '艺术',
+                # '理学',
+                '文学', '资源',
+                # '公管',
+                '化工', '体育', '安德', '城市', '马院']
+
+# college_list = ['信控']
 
 with open(r'src/250102.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -18,7 +24,33 @@ text_de_time = re.sub(pattern, "", text)
 # print(text_de_time)
 
 chat_infos = [i.strip() for i in text_de_time.split("\n\n")]
+chat_infos_check = []
 
+# 排除conflict_college之外的学院
+print(len(chat_infos))
+for college in college_list:
+    for info in chat_infos:
+        if college in info:
+            print(college + "---" + info)
+            chat_infos_check.append(info)
+    print("*******************************")
+print(len(chat_infos_check))
+
+# 去除chat_infos中包含的chat_infos_check的项
+for info in chat_infos_check:
+    chat_infos.remove(info)
+print(len(chat_infos))
+
+# 处理conflict_college中的学院
 for info in chat_infos:
-    print(info)
-    print("-" * 50)
+    for i in range(3):
+        if conflict_college[0] not in info:
+            continue
+        if conflict_college[i] in info:
+            continue
+        else:
+            print(conflict_college[i - 1] + "---" + info)
+            break
+
+# for info in chat_infos:
+#     print(info)
